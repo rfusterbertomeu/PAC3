6c2ecc502952805fcd3fbb3af2fa7efbbe97328b
Roser Fuster Bertomeu <rfuster@uoc.edu>
2023-12-12 18:44:21 +0100

    pagines PAC2

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,12 +5,38 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
-
 /**
  * Write any other JavaScript below
  */
+const enllac_header = document.getElementsByClassName("header__enllac");
+const enllac_portada = document.getElementsByClassName("portada__enllac");
+const enllac_footer = document.getElementsByClassName("footer__enllac--menu");
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+Array.from(enllac_header).forEach(function (enllac) {
+  enllac.addEventListener("click", function () {
+    const target = enllac.getAttribute("data-target");
+    mostra(target);
+  });
+});
+Array.from(enllac_portada).forEach(function (enllac) {
+  enllac.addEventListener("click", function () {
+    const target = enllac.getAttribute("data-target");
+    mostra(target);
+  });
+});
+Array.from(enllac_footer).forEach(function (enllac) {
+  enllac.addEventListener("click", function () {
+    const target = enllac.getAttribute("data-target");
+    mostra(target);
+  });
+});
+
+function mostra(id) {
+  document.querySelectorAll(".pagina").forEach(function (pagina) {
+    pagina.style.display = "none";
+  });
+
+  document.getElementById(id).style.display = "flex";
+  document.getElementById(id).style.flexDirection = "column";
+  document.getElementById(id).style.alignItems = "center";
+}
