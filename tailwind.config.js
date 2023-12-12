eb98f27446fa0f9d0271263c4077db2953cb0420
Roser Fuster Bertomeu <rfuster@uoc.edu>
2023-12-12 20:36:00 +0100

    header i @apply .card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-600": "#000078",
+        primary: "#be9bff",
+        secondary: "#4c177a",
+      },
+      fontFamily: {
+        text: "Montserrat Variable, sans-serif",
+        title: "Outfit Variable, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
