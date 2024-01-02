5991849373fedc7dc508404dbb49f84869d41897
Roser Fuster Bertomeu <rfuster@uoc.edu>
2024-01-02 20:37:48 +0100

    modificació colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,21 +5,26 @@ module.exports = {
     extend: {
       colors: {
         uoc: "#000078",
-        primary: "#be9bff",
+        primary: {
+          transparent: "#ede9fe",
+          light: "#f2ebff",
+          DEFAULT: "#be9bff",
+        },
         secondary: "#4c177a",
       },
       fontFamily: {
         text: "Montserrat Variable, sans-serif",
         title: "Outfit Variable, sans-serif",
-      },fontSize:{
-      'xxs':['0.7rem', '1rem'],
-     },     
+      },
+      fontSize: {
+        xxs: ["0.7rem", "1rem"],
+      },
     },
     screens: {
-      'sm': { 'max': '767px' },
-      'md': { 'min': '768px', 'max': '1023px' },
-      'lg': '1024px'
-    }
+      sm: { max: "767px" },
+      md: { min: "768px", max: "1023px" },
+      lg: "1024px",
+    },
   },
-  plugins: [ require('@tailwindcss/container-queries')],
+  plugins: [require("@tailwindcss/container-queries")],
 };
